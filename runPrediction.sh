846400005fecbf08da8f1067b197a09e3aee09e4
Robert Meissner <robert.meissner@codecentric.de>
2022-05-18 11:26:45 +0200

    Setting up CI/CD
    Changing base image

--- runPrediction.sh
+++ runPrediction.sh
@@ -1,3 +1,3 @@
 
 
-docker run -v `pwd`/data:/data -v `pwd`/src:/scr wlo-cuda:1.0 /usr/bin/python3 /scr/predict.py /data/wirlernenonline.oeh3.h5 /data/wirlernenonline.oeh3.npy  /data/wirlernenonline.oeh3.pickle "$1"
+docker run -v `pwd`/data:/data -v `pwd`/src:/scr docker.edu-sharing.com/projects/oeh-redaktion/edusharing-projects-oeh-redaktion-yovisto-classification:dev /usr/bin/python3 /scr/predict.py /data/wirlernenonline.oeh3.h5 /data/wirlernenonline.oeh3.npy  /data/wirlernenonline.oeh3.pickle "$1"
